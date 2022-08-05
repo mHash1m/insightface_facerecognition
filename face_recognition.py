c83ca5c118a1a89c2be8907a8d0229441a488f7c
Hashim <58783659+mHash1m@users.noreply.github.com>
2022-08-05 22:07:46 +0500

    Add webcam support

--- face_recognition.py
+++ face_recognition.py
@@ -73,6 +73,46 @@ def run_for_video(app, target, register):
     cap.release()
     cv2.destroyAllWindows()
 
+def run_for_webcam(app, target, register, show):
+    # define a video capture object
+    vid = cv2.VideoCapture(int(target))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    file_name = Path(target).stem
+    out = cv2.VideoWriter(f"./results/webcam_output.mp4", fourcc, 5, (640,480))
+    while(True):
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+        faces = app.get(frame)
+        for face in faces:
+            face_emb = face["embedding"]
+            face_age = face["age"]
+            gender = "M" if face["gender"] == 1 else "F"
+            highest_sim = ("mike", -1)
+            for key, val in register.items():
+                cos_sim = sim(face_emb, val)
+                if  cos_sim > highest_sim[1]:
+                    highest_sim = [key, cos_sim]
+            x1, y1, x2, y2 = s = [int(px) for px in face["bbox"]]
+            cv2.rectangle(frame,(x1,y1),(x2,y2),(127,69,18),3)
+            cv2.putText(frame,f"{gender}:{face_age}",(x1,y2+24),5,0.7,(255,178,102))
+            # print("Averaged =", highest_sim)
+            if highest_sim[1] >= 0.3:
+                cv2.putText(frame,f"{highest_sim[0]}[{highest_sim[1]}]",(x1,y2+12),5,0.7,(255,178,102))
+        # Display the resulting frame
+        if show:
+            cv2.imshow('frame', frame)
+        out.write(frame)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
 def run_for_image(app, target, register):
     file_name = Path(target).stem
     img = cv2.imread(target)
@@ -97,17 +137,20 @@ def run_for_image(app, target, register):
 parser=argparse.ArgumentParser()
 parser.add_argument("-t", "--task", type=str, required=True, help="'register', 'inference image' or 'inference video'")
 parser.add_argument("-tg", "--target", type=str, required=True, help="path to vid, image or dir")
+parser.add_argument("-s", "--show", default=False, action='store_true', help="show cam feed inference")
 args=parser.parse_args()
 task = args.task
 target = args.target
+show = args.show
 
-app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+app = FaceAnalysis(providers=['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(640, 640))
 
 try:
     os.mkdir("./results")
 except OSError as error:
     print(str(error)[10:])
+
 if task == "register":
     register_faces(app, target)
 elif task == "inference image":
@@ -116,5 +159,8 @@ elif task == "inference image":
 elif task == "inference video":
     register = json.load(open("./registered_faces.json"))
     run_for_video(app, target, register)
+elif task == "inference cam":
+    register = json.load(open("./registered_faces.json"))
+    run_for_webcam(app, target, register, show)
 else:
     print("UNKNOWN TASK, should be 'register', 'inference image' or 'inference video'")
